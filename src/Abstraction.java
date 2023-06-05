d573d278d82d381969aec639a0d99b577064fd04
sharan998 <sharan@jayeesha.com>
2023-06-04 23:12:07 -0400

    Day 2

---
+++ src/Abstraction.java
@@ -0,0 +1,17 @@
+abstract  class Department{
+    public abstract int getDepartmentSize(int x);
+}
+
+public class Abstraction extends Department {
+
+    public static void main(String[] args){
+        Abstraction dep = new Abstraction();
+        int val =dep.getDepartmentSize(10);
+        System.out.println(val);
+    }
+
+    @Override
+    public int getDepartmentSize(int x) {
+        return x;
+    }
+}
