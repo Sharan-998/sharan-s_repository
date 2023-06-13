acdf09a3adf78676976fee23319a2d464db6cc2d
sharan998 <sharan@jayeesha.com>
2023-06-13 16:25:03 -0400

    Exceptions 06/13/2023

---
+++ src/Exceptions.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exceptions {
+
+
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();
+            try {
+                int a = convertToInt(s);
+                System.out.println("Integer value of "+ s + " : " +a);
+            }catch (Exception e){
+                System.out.println(e.toString());
+            }
+        }
+        public static int convertToInt(String s){
+            return Integer.parseInt(s);
+        }
+}
