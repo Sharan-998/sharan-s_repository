bb6d322c8f320269bf60fb2fce46058d8d9ab184
sharan998 <sharan@jayeesha.com>
2023-06-14 14:55:09 -0400

    Threads 06-14-2023

---
+++ src/Day_12/Multi.java
@@ -0,0 +1,13 @@
+package Day_12;
+
+class Multi extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String args[]){
+        Multi t1=new Multi();
+        t1.start();
+    }
+}  
